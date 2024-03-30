5b5c271e8e4ca75acfc7f1b39b5fd2bfcabf10fe
Vlad Leontescu <lvlad27@gmail.com>
2024-03-30 13:57:25 +0200

    feat: start step 3

--- src/app.js
+++ src/app.js
@@ -2,18 +2,36 @@
 
 import fs from "fs";
 import readline from "readline";
-import { argv } from "process";
+import { argv, stdin } from "process";
+import assert from "assert";
 
-const filepath = argv[2];
+const myCat = () => {
+	console.log(argv);
+	const filepath = argv[2];
+	const input = [undefined, "-"];
 
-const myCat = (filepath) => {
-	fs.readFile(filepath, "utf8", (err, data) => {
-		if (err) {
-			console.error(err.message);
-			return;
-		}
-		console.log(data);
-	});
+	// read from stdin
+	if (input.includes(argv[2])) {
+		stdin.setEncoding("utf-8");
+		stdin.on("data", (chunk) => {
+			assert.equal(typeof chunk, "string");
+			console.log(chunk);
+		});
+	} else {
+		// read from file
+		fs.readFile(argv[2], "utf8", (err, data) => {
+			if (err) {
+				console.error(err.message);
+				return;
+			}
+			console.log(data);
+		});
+	}
 };
 
-myCat(filepath);
+// argv is an array
+// I need to concatanate one or more files. Use reduce to
+if (argv.length > 2) {
+}
+
+myCat();
